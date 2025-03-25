74431e3ea1216557bed7f201566007ce4bf578b5
AlanTeixido <alan.teixido.7e7@itb.cat>
2025-03-25 16:08:54 +0100

    chore: Update navigation links and component names

--- examenAlanm14_UF1/src/App.vue
+++ examenAlanm14_UF1/src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <RouterLink to="/">Home</RouterLink> |
     <RouterLink to="/pagina1">Pàgina 1</RouterLink> |
+    <RouterLink to="/pagina2">Pàgina 2</RouterLink> |
     <RouterLink to="/pagina3">Pàgina 3</RouterLink>
   </nav>
   <RouterView />
--- examenAlanm14_UF1/src/router/index.js
+++ examenAlanm14_UF1/src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', name: 'home', component: () => import('../views/Home.vue') },
-  { path: '/pagina1', name: 'pagina1', component: () => import('../views/Pagina1.vue') },
-  { path: '/pagina2/:id', name: 'pagina2', component: () => import('../views/Pagina2.vue') },
-  { path: '/pagina3', name: 'pagina3', component: () => import('../views/Pagina3.vue') }
+  { path: '/', name: 'home', component: () => import('../views/HomeView.vue') },
+  { path: '/pagina1', name: 'pagina1', component: () => import('../views/PaginaUn.vue') },
+  { path: '/pagina2/:id', name: 'pagina2', component: () => import('../views/PaginaDigues.vue') },
+  { path: '/pagina3', name: 'pagina3', component: () => import('../views/PaginaTres.vue') }
 ]
 
 const router = createRouter({
